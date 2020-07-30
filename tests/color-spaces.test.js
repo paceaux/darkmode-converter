c82f9a2e58511981b5315b8817424587637ef3d6
Frank Taylor <Frank.Taylor@exlrt.com>
2020-07-30 11:40:09 -0500

    sets toHsla methods to return numbers

--- tests/color-spaces.test.js
+++ tests/color-spaces.test.js
@@ -26,30 +26,38 @@ test('rgbToHex, ', () => {
 
 test('rgbToHsl, ', () => {
     const testColor = 'rgb(255,0,0)';
+    const result = ColorSpaces.rgbToHsl(testColor);
 
-    expect(ColorSpaces.rgbToHsl(testColor)).toBe('hsl(0,100%,50%)');
+    expect(result[0]).toBe(0);
+    expect(result[1]).toBe(1);
+    expect(result[2]).toBe(0.5);
 });
 
 test('rgbaToHsla, ', () => {
     const testColor = 'rgba(255,0,0,1)';
+    const result = ColorSpaces.rgbaToHsla(testColor);
 
-    expect(ColorSpaces.rgbaToHsla(testColor)).toBe('hsla(0,100%,50%,1)');
+    expect(result[0]).toBe(0);
+    expect(result[1]).toBe(1);
+    expect(result[2]).toBe(0.5);
+    expect(result[3]).toBe(1);
 });
 
 test('hexToHsl, ', () => {
     const testColor = '#ff0000';
+    const result = ColorSpaces.hexToHsl(testColor);
 
-    expect(ColorSpaces.hexToHsl(testColor)).toBe('hsl(0,100%,50%)');
+    expect(result[0]).toBe(0);
+    expect(result[1]).toBe(1);
+    expect(result[2]).toBe(0.5);
 });
 
 test('hexAToHslA, ', () => {
     const testColor = '#ff0000ff';
+    const result = ColorSpaces.hexaToHsla(testColor);
 
-    expect(ColorSpaces.hexaToHsla(testColor)).toBe('hsla(0,100%,50%,1.000)');
-});
-
-test('rgbaToHsla, ', () => {
-    const testColor = 'rgba(255,0,0,1)';
-
-    expect(ColorSpaces.rgbaToHsla(testColor)).toBe('hsla(0,100%,50%,1)');
+    expect(result[0]).toBe(0);
+    expect(result[1]).toBe(1);
+    expect(result[2]).toBe(0.5);
+    expect(result[3]).toBe(1);
 });
