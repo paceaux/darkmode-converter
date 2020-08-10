0905374042a022060029bb5c2e57975332525680
Frank Taylor <Frank.Taylor@exlrt.com>
2020-08-10 13:29:00 -0500

    adds conversions for rgb, hsl

--- tests/color-class.test.js
+++ tests/color-class.test.js
@@ -19,4 +19,42 @@ describe('Color Class', () => {
             expect(red.colorParts).toEqual(['0xff', '0x00', '0x00']);
         });
     });
+    describe('Starting with Hex', () => {
+        const red = new Color('#f00');
+        test('color in hex has rgb _parts_', () => {
+            expect(red).toHaveProperty('rgbParts');
+            expect(red.rgbParts).toEqual([255, 0, 0]);
+        });
+        test('color in hex has rgb _parts_', () => {
+            expect(red).toHaveProperty('rgb');
+            expect(red.rgb).toEqual('rgb(255,0,0)');
+        });
+        test('color in hex has hsl _parts_', () => {
+            expect(red).toHaveProperty('hslParts');
+            expect(red.hslParts).toEqual([0, 1, 0.5]);
+        });
+        test('color in hex has hsl', () => {
+            expect(red).toHaveProperty('hsl');
+            expect(red.hsl).toEqual('hsl(0,100%,50%)');
+        });
+    });
+    describe('Starting with rgb', () => {
+        const red = new Color('rgb(255,0,0)');
+        test('color in rgb has hex _parts_', () => {
+            expect(red).toHaveProperty('hexParts');
+            expect(red.hexParts).toStrictEqual(['0xff', '0x00', '0x00']);
+        });
+        test('color in rgb has hex', () => {
+            expect(red).toHaveProperty('hex');
+            expect(red.hex).toEqual('#ff0000');
+        });
+        test('color in rgb has hsl _parts_', () => {
+            expect(red).toHaveProperty('hslParts');
+            expect(red.hslParts).toEqual([0, 1, 0.5]);
+        });
+        test('color in rgb has hsl', () => {
+            expect(red).toHaveProperty('hsl');
+            expect(red.hsl).toEqual('hsl(0,100%,50%)');
+        });
+    });
 });
